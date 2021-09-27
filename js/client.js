f1d471c8f70e7c4ed1df76c9d2465d4d72bc9e58
Raghav1209 <raghavghai170@gmail.com>
2021-09-27 17:59:22 +0530

    initial commit

---
+++ js/client.js
@@ -0,0 +1,40 @@
+const socket = io('http://localhost:8000');
+
+const form = document.getElementById('send-container');
+const messageInput = document.getElementById('messageinp');
+const messageContainer = document.querySelector(".container");
+var audio = new Audio('ting.wav');
+
+const append = (message,position)=>{
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    messageElement.classList.add('message')
+    messageElement.classList.add(position)
+    messageContainer.append(messageElement);
+    if(position=='left'){
+        audio.play();
+    }
+}
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const message = messageInput.value;
+    append(`You: ${message}`,'Right');
+    socket.emit('send',message);
+    messageInput.value='';
+})
+
+const Name = prompt("Enter your Name to join");
+socket.emit('new-user-joined', Name);
+
+socket.on('user-joined' , Name=>{
+    append(`${Name} joined the chat`, 'Right');
+})
+
+socket.on('receive' , data=>{
+    append(`${data.Name}: ${data.message}`,'left');
+})
+
+socket.on('left' , Name=>{
+    append(`${Name} left the chat`,'Right');
+})
